c833eab756e73b22ac64747b55629609b6700bdf
medaminechihab97 <med.chihab97@gmail.com>
2024-07-28 21:53:12 +0100

    first SQL queries

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,13 @@
 USE alx_book_store;
 
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'books';
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
