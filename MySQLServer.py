3d8763367ef55a52aa6ac64951ea23c7d62509a9
medaminechihab97 <med.chihab97@gmail.com>
2024-07-28 21:33:20 +0100

    first SQL queries

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,3 +1,5 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(host="localhost", user="root", passwd="1234")
+mycusor = mydb.cursor()
+mycusor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
