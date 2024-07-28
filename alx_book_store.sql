cbf6ed5a691706d7c79d0b436534530cd499f52a
medaminechihab97 <med.chihab97@gmail.com>
2024-07-28 21:15:31 +0100

    nothing

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,1 +1,36 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store
+CREATE TABLE Books (
+book_id PRIMARY key,
+title varchar(130),
+author_id FOREIGN KEY,
+price DOUBLE,
+);
+
+CREATE TABLE Authors (
+autour_id PRIMARY KEY,
+author_name varchar(215)
+);
+
+CREATE TABLE Customers (
+custumer_id PRIMARY KEY,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+
+);
+
+CREATE TABLE Orders (
+order_id PRIMARY KEY,
+customer_id FOREIGN KEY,
+order_date DATE,
+
+
+);
+CREATE TABLE Order_Details (
+orderdetailid PRIMARY KEY,
+order_id FOREIGN KEY,
+book_id FOREIGN KEY referencing Books table,
+quantity DOUBLE,
+
+
+);
